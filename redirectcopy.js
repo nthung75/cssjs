7aec65d4c7aa8c0ceebd71ec2228bd4feab0457a
nthung75 <90054594+nthung75@users.noreply.github.com>
2021-09-22 17:58:09 +0700

    Add files via upload

--- redirectcopy.js
+++ redirectcopy.js
@@ -1,8 +1,7 @@
 function FSAddOriginalLink(){
     var body_element = document.getElementsByTagName('body')[0];
     var selection;
-    selection = window.getSelection();
-    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='http://faceseo.vn'>http://faceseo.vn"+document.location.pathname+"</a>";
+    selection = window.getSelection();https://vtdimexco.vn/'>https://vtdimexco.vn/"+document.location.pathname+"</a>";
     var copytext = pagelink;
     var newdiv = document.createElement('div');
     newdiv.style.position='absolute';
