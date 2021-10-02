9c54296f62ed498e57e5ac21e830d601045534e6
nthung75 <90054594+nthung75@users.noreply.github.com>
2021-10-02 09:29:53 +0700

    Add files via upload

---
+++ randompopup.js
@@ -0,0 +1,6 @@
+<script type="text/javascript">
+var linkss = new Array('string1','string2');
+var max = linkss.length;
+var num = Math.floor((Math.random() * max));
+document.writeln(linkss[num]);
+</script>
